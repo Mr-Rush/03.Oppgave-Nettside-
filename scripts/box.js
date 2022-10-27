e0d0ab7eb487ecec4d3e337f082cc35ee1bb2ee6
Rushed <mister.rush.dev@gmail.com>
2022-10-27 21:01:51 +0200

    gallery

--- scripts/box.js
+++ scripts/box.js
@@ -19,4 +19,4 @@ const addWord = (text, index) => subtitle.appendChild(createWord(text, index));
 
 const createSubtitle = (text) => text.split(" ").map(addWord);
 
-createSubtitle("Heyooo I'm Odin and im just casually writing this about page for a school project that no one else will read probably not even the teacher will read this lmao. Well this is an abou me soooo Im a 16 year old IM studen at Gand VGS. I work at a convinience store every wednesday I primarily write in English which you have noticed. My wpm is way higher in English as its in the 90/100wpm but lower in Norwegian. ");
+createSubtitle("Heyooo I'm Odin and im just casually writing this about page for a school project that no one else will read probably not even the teacher will read this lmao. Well this is an about me soooo Im a 16 year old IM student at Gand VGS. I work at a convinience store every wednesday I primarily write in English which you have noticed. My wpm is way higher in English as its in the 90/100wpm but lower in Norwegian. ");
